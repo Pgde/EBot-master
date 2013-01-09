dd0c45931ed70fb1bb7eb7c1433845f18f6097e8
DerM <pimpergrimper@kaymail.de>
2013-01-09 15:56:56 +0100

    Add Skillcontroler
    Add Skillcontroller

---
+++ Controllers/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EveModel;
+using MySql.Data.MySqlClient;
+
+namespace Controllers
+{
+
+    public class SkillController : BaseController
+    {
+        long _destinationId, _currentLocation, _currentDestGateId;
+        bool _waitforsessionChange;
+        enum TravelStates { Initialise, Start, Travel, ArrivedAtDestination, sqlsettime, sqlstarttime, sqlcheck, sqltimecheck }
+
+
+
+
+
+
+        ////////////////////////////////////////////////////////
+        ///////////           VARIABLEN        ////////
+
+
+        ////////////////////////////////////////////////////////
+        ///////////           VARIABLEN        ////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+        TravelStates _state;
+
+        public SkillController()
+        {
+            Frame.Log("Starting a new SkillController");
+        }
+
+
+        public override void DoWork()
+        {
+            if (IsWorkDone || _localPulse > DateTime.Now || Frame.Client.Session.NextSessionChange > DateTime.Now)
+            {
+                return;
+            }
+            switch (_state)
+            {
+                case TravelStates.Initialise:
+
+
+            List<EveSkill> neueskill = Frame.Client.GetMySkills();
+            List<EveQskill> neueQskill = Frame.Client.GetMyQueue();
+
+            foreach (EveSkill tmp in neueskill)
+            {
+                Frame.Log("Skill = " + tmp.Name + ",  " + "Lvl = " + tmp.Skilllvl + " , " + "skillid  = " + tmp.typeID);
+            }
+            Frame.Log("................");
+            Frame.Log("................");
+            Frame.Log("................");
+            Frame.Log("................");
+            foreach (EveQskill tmp in neueQskill)
+            {
+                Frame.Log("Skill = " + tmp._name + ",  " + "Lvl = " + tmp.Skilllvl + " , " + "skillid  = " + tmp.typeID);
+            }
+            Frame.Log("................");
+            Frame.Log("................");
+            Frame.Log("................");
+            Frame.Log("................");
+
+
+            List<EveSkill> tooskill;
+
+                     _state = TravelStates.Start;
+                    break;
+
+
+                case TravelStates.Start:
+
+
+
+
+
+                    break;
+            }
+             
+            
+        }
+    }
+}
+
