b8ba4b20b72dcd732d9b3bc897cf520bc599e248
St St <darkness99@gmx.net>
2013-01-19 09:22:13 +0100

    added barebone maincontroller

---
+++ Controllers/MainController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EveModel;
+using global::Controllers.states;
+
+namespace Controllers
+{
+
+    public class MainController : BaseController
+    {
+
+
+
+        ////////////////////////////////////////////////////////
+        ///////////           VARIABLEN        ////////
+
+        DateTime errorwait = DateTime.Now;
+
+
+
+
+
+
+
+
+
+
+        public MainController()
+        {
+            Frame.Log("Starting a new MainController");
+
+        }
+
+
+        public override void DoWork()
+        {
+            if (IsWorkDone || _localPulse > DateTime.Now || Frame.Client.Session.NextSessionChange > DateTime.Now)
+            {
+                return;
+            }
+
+
+            switch (_States.maincontrollerStates)
+            {
+                case maincontrollerStates.Idle:
+
+                    _localPulse = DateTime.Now.AddMilliseconds(GetRandom(20000, 35000));
+                    break;
+
+                case maincontrollerStates.Error:
+
+                    _localPulse = DateTime.Now.AddMilliseconds(GetRandom(200000000, 350000000));      //dirty 
+                    Frame.Log("Error tutstates");
+                    _States.tutstates = tutstates.Error;
+                    break;
+
+                case maincontrollerStates.wait:
+
+                    _localPulse = DateTime.Now.AddMilliseconds(GetRandom(1000, 2500));
+                    break;
+
+
+            }
+        }
+    }
+}
+
---
+++ Controllers/states/MainControllerstates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Controllers.states
+{
+
+    public enum maincontrollerStates
+    {
+        Idle,
+        Error,
+        wait
+    }
+
+}
+
